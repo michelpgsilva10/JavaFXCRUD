e5499c2d6d030ea87fba148a75fff3cfc8654f22
Michel <Michel@Ruan>
2015-09-01 00:46:39 -0400

    Ajustes da classe de Produto

--- src/mercado/model/Produto.java
+++ src/mercado/model/Produto.java
@@ -1,5 +1,22 @@
 package mercado.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Produto {
 
+	private final IntegerProperty codigo;
+	private final StringProperty nome;
+	
+	public Produto() {
+		this(null, null);
+	}
+	
+	public Produto(Integer codigo, String nome) {
+		this.codigo = new SimpleIntegerProperty(codigo.intValue());
+		this.nome = new SimpleStringProperty(nome);
+	}
+	
 }
